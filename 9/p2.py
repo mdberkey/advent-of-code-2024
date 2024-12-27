457e442c284c9b709d5ff6e38f21578265ddb308
mdberkey <mberkey@wisc.edu>
2024-12-26 23:37:22 -0500

    update day 9 p2 correct test input

--- 9/p2.py
+++ 9/p2.py
@@ -10,55 +10,64 @@ def get_disk_list(diskmap):
         disk_list.extend(["."] * free_space)
         id += 1
     
-    return disk_list
+    return disk_list, id
 
-def compress_disk_list(disk_list):
-    left = 0
-    right = len(disk_list) - 1
+def compress_disk_list(disk_list, num_files):
 
-    while left < right:
-        while left < right and disk_list[right] == ".":
-            right -= 1
-        f_end = f_start = right
-        while disk_list[f_start] == disk_list[f_end]:
-            f_start -= 1
-        f_start += 1
-        f_size = f_end - f_start + 1
+    def get_next_free_block(disk_list, i):
+        free_start = free_end = i
+        # move to start of free space
+        while disk_list[free_start] != ".":
+            free_start += 1
+        free_end = free_start
+        # get end of free space (exclusive)
+        while free_end < len(disk_list) and disk_list[free_end] == ".":
+            free_end += 1 
+        return free_start, free_end
 
-        while left < right and disk_list[left] != ".":
-                left += 1
-        s_start = s_end = left
+    def get_next_file_block(disk_list, i):
+        block_start = block_end = i
+        # move to end of block (exclusive)
+        while disk_list[block_end] == ".":
+            block_end -= 1
+        block_end += 1
+        # get start of block
+        block_start = block_end - 1
+        while disk_list[block_start] == disk_list[block_end-1]:
+            block_start -= 1 
+        block_start += 1
+        return block_start, block_end
 
-        # fix
-        while s_end < f_start and s_end - s_start + 1 < f_size:
-            while s_end < f_start and disk_list[s_end] != ".":
-                s_end += 1
-                s_start = s_end
-            
-            while s_end < f_start and disk_list[] == ".":
-                left += 1
-                s_end = left 
+    # iterate through file blocks, on each, iterate open free space and attempt move.
+    next_bval = len(disk_list) - 1
+    for _ in range(num_files):
+        block_start, block_end = get_next_file_block(disk_list, next_bval)
+        next_bval = block_start - 1
+        block_size = block_end - block_start
 
-        if left >= right or s_end - s_start <= f_size:
+        free_size = -1
+        next_fval = 0
+        while free_size < block_size and next_fval < len(disk_list) and next_fval < block_start:
+            free_start, free_end = get_next_free_block(disk_list, next_fval)
+            next_fval = free_end
+            free_size = free_end - free_start
+        
+        # if no large enough free space, continue to other blocks        
+        if free_size < block_size:
             continue
 
-        for i in range(f_size):
-            disk_list[f_start+i], disk_list[s_start+i] = disk_list[s_start+i], disk_list[f_start+i]
-
-        left += 1
-        right -= 1
-    print(left, right)
+        # swap free space with block
+        for i in range(block_size):
+             disk_list[free_start+i], disk_list[block_start+i] = disk_list[block_start+i], disk_list[free_start+i]
 
     return disk_list  
 
 if __name__ == "__main__":
-    lines = open("9/i0").read().splitlines()
-    disk_list = get_disk_list(lines[0])
-    res_disk_list = compress_disk_list(disk_list)
-    print(disk_list)
+    lines = open("9/i1").read().splitlines()
+    disk_list, num_files = get_disk_list(lines[0])
+    res_disk_list = compress_disk_list(disk_list, num_files)
     res = 0
     for i, id in enumerate(res_disk_list):
-        if id == ".":
-            break
-        res += i * id
-    print(res)
+        if id != ".":
+            res += i * id
+    print(res) # 6636716706623 answer too high, but getting test input right on 2858... 
