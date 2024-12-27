ae6632d8966987f2deb330187c67cc560d1977bb
mdberkey <mberkey@wisc.edu>
2024-12-27 12:50:20 -0700

    complete day 9 p2

--- 9/p2.py
+++ 9/p2.py
@@ -17,7 +17,7 @@ def compress_disk_list(disk_list, num_files):
     def get_next_free_block(disk_list, i):
         free_start = free_end = i
         # move to start of free space
-        while disk_list[free_start] != ".":
+        while free_start < len(disk_list) and disk_list[free_start] != ".":
             free_start += 1
         free_end = free_start
         # get end of free space (exclusive)
@@ -28,21 +28,27 @@ def compress_disk_list(disk_list, num_files):
     def get_next_file_block(disk_list, i):
         block_start = block_end = i
         # move to end of block (exclusive)
-        while disk_list[block_end] == ".":
+        while block_end >= 0 and disk_list[block_end] == ".":
             block_end -= 1
         block_end += 1
         # get start of block
         block_start = block_end - 1
-        while disk_list[block_start] == disk_list[block_end-1]:
+        while block_start >= 0 and disk_list[block_start] == disk_list[block_end-1]:
             block_start -= 1 
         block_start += 1
         return block_start, block_end
 
     # iterate through file blocks, on each, iterate open free space and attempt move.
     next_bval = len(disk_list) - 1
+    seen_ids = set()
     for _ in range(num_files):
         block_start, block_end = get_next_file_block(disk_list, next_bval)
         next_bval = block_start - 1
+        
+        while next_bval >= 0 and disk_list[block_start] in seen_ids:
+            block_start, block_end = get_next_file_block(disk_list, next_bval)
+            next_bval = block_start - 1
+        seen_ids.add(disk_list[block_start])
         block_size = block_end - block_start
 
         free_size = -1
@@ -53,13 +59,12 @@ def compress_disk_list(disk_list, num_files):
             free_size = free_end - free_start
         
         # if no large enough free space, continue to other blocks        
-        if free_size < block_size:
+        if free_size < block_size or free_start > block_end - 1:
             continue
-
+            
         # swap free space with block
         for i in range(block_size):
              disk_list[free_start+i], disk_list[block_start+i] = disk_list[block_start+i], disk_list[free_start+i]
-
     return disk_list  
 
 if __name__ == "__main__":
@@ -70,4 +75,4 @@ if __name__ == "__main__":
     for i, id in enumerate(res_disk_list):
         if id != ".":
             res += i * id
-    print(res) # 6636716706623 answer too high, but getting test input right on 2858... 
+    print(res)
